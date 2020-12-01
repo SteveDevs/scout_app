5eea0a17cf66d6f7da9657d98ddd0809cd6fdca2
SteveDevs <29339618+SteveDevs@users.noreply.github.com>
2020-12-01 22:09:54 +0200

    Update ControllerBase.php
    format correction

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -22,9 +22,39 @@ class ControllerBase extends Controller
      */
     private function config_validations()
     {
-        return ['min_chars' => ['telephone' => '12', 'password' => '6', 'valid_string' => '1', 'valid_long_string' => '20', ], 'max_chars' => ['telephone' => '12', 'password' => '15', 'valid_string' => '25', 'valid_long_string' => '200', ], 'regex' => ['telephone' => '/\+44 [0-9]+/', 'password' => '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})', 'valid_string' => '/[\'\/~`\!@#\$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\\]/'], 'present' => ['username' => 'Username required', 'email' => 'Email required', 'password' => 'Password required', ], 'valid' => ['email' => 'Invalid Email', 'telephone' => "Invalid Telephone", 'username' => "Invalid Username", 'password' => "Invalid Password", 'name' => "Invalid Name", 'surname' => "Invalid Surname", 'job_title' => "Invalid Job Title", 'bio' => "Invalid Bio",
-
-        ]];
+        return [
+            'min_chars' => [
+                'telephone' => '12', 
+                'password' => '6', 
+                'valid_string' => '1', 
+                'valid_long_string' => '20', 
+            ], 
+            'max_chars' => [
+                'telephone' => '12', 
+                'password' => '15', 
+                'valid_string' => '25', 
+                'valid_long_string' => '200', 
+            ], 
+            'regex' => [
+                'telephone' => '/\+44 [0-9]+/', 
+                'password' => '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})', 
+                'valid_string' => '/[\'\/~`\!@#\$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\\]/'], 
+            'present' => [
+                'username' => 'Username required', 
+                'email' => 'Email required', 
+                'password' => 'Password required', 
+            ], 
+            'valid' => [
+                'email' => 'Invalid Email', 
+                'telephone' => "Invalid Telephone", 
+                'username' => "Invalid Username", 
+                'password' => "Invalid Password", 
+                'name' => "Invalid Name", 
+                'surname' => "Invalid Surname", 
+                'job_title' => "Invalid Job Title", 
+                'bio' => "Invalid Bio",
+            ]
+        ];
     }
 
     /**
@@ -34,7 +64,61 @@ class ControllerBase extends Controller
      */
     private function load_fields()
     {
-        return ['username' => ['id' => 'username', 'field' => null, 'regex' => $this->config_validations() ['regex']['valid_string'], 'present' => $this->config_validations() ['present']['username'], 'message' => $this->config_validations() ['valid']['username'], ], 'email' => ['id' => 'email', 'field' => null, 'email' => $this->config_validations() ['valid']['email'], 'message' => $this->config_validations() ['valid']['username'], ], 'password' => ['id' => 'password', 'field' => null, 'present' => $this->config_validations() ['present']['password'], 'message' => $this->config_validations() ['valid']['password'], ], 'telephone' => ['id' => 'telephone', 'field' => null, 'required' => false, 'message' => $this->config_validations() ['valid']['telephone'], ], 'name' => ['id' => 'name', 'field' => null, 'required' => false, 'regex' => $this->config_validations() ['regex']['valid_string'], 'message' => $this->config_validations() ['valid']['name'], ], 'surname' => ['id' => 'surname', 'field' => null, 'required' => false, 'regex' => $this->config_validations() ['regex']['valid_string'], 'message' => $this->config_validations() ['valid']['surname'], ], 'job_title' => ['id' => 'job_title', 'field' => null, 'required' => false, 'regex' => $this->config_validations() ['regex']['valid_string'], 'message' => $this->config_validations() ['valid']['job_title'], ], 'bio' => ['id' => 'bio', 'field' => null, 'required' => false, 'regex' => $this->config_validations() ['regex']['valid_string'], 'message' => $this->config_validations() ['valid']['bio'], ], ];
+        return [
+            'username' => [
+                'id' => 'username', 
+                'field' => null, 
+                'regex' => $this->config_validations() ['regex']['valid_string'], 
+                'present' => $this->config_validations() ['present']['username'], 
+                'message' => $this->config_validations() ['valid']['username'], 
+            ], 
+            'email' => [
+                'id' => 'email', 
+                'field' => null, 
+                'email' => $this->config_validations() ['valid']['email'], 
+                'message' => $this->config_validations() ['valid']['username'], 
+            ], 
+            'password' => [
+                'id' => 'password', 
+                'field' => null, 
+                'present' => $this->config_validations() ['present']['password'], 
+                'message' => $this->config_validations() ['valid']['password'], 
+            ], 
+            'telephone' => [
+                'id' => 'telephone', 
+                'field' => null, 
+                'required' => false, 
+                'message' => $this->config_validations() ['valid']['telephone'], 
+            ], 
+            'name' => [
+                'id' => 'name', 
+                'field' => null, 
+                'required' => false, 
+                'regex' => $this->config_validations() ['regex']['valid_string'], 
+                'message' => $this->config_validations() ['valid']['name'], 
+            ], 
+            'surname' => [
+                'id' => 'surname', 
+                'field' => null, 
+                'required' => false, 
+                'regex' => $this->config_validations() ['regex']['valid_string'], 
+                'message' => $this->config_validations() ['valid']['surname'], 
+            ], 
+            'job_title' => [
+                'id' => 'job_title', 
+                'field' => null, 
+                'required' => false, 
+                'regex' => $this->config_validations() ['regex']['valid_string'], 
+                'message' => $this->config_validations() ['valid']['job_title'], 
+            ], 
+            'bio' => [
+                'id' => 'bio', 
+                'field' => null, 
+                'required' => false, 
+                'regex' => $this->config_validations() ['regex']['valid_string'], 
+                'message' => $this->config_validations() ['valid']['bio'], 
+            ], 
+        ];
     }
 
     /**
